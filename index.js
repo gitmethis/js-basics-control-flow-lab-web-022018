6090a468b81b1aa72298428dcc5200dd6a406a61
Chase <chaseit12@gmail.com>
2018-03-31 16:02:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 // Write your code in this file!
 
 
-function scuberGreetingForFeet(distance){
-  switch (distance){
-    case (distance < 400):
-      return 'This one is on me!'
-      break;
-    default:
-      return 'nothing'
-  }
-}
