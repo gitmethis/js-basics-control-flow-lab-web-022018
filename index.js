f772e3600b44cea93522ec7b3ad28f0322e7dbe2
Chase <chaseit12@gmail.com>
2018-03-31 16:04:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ function scuberGreetingForFeet(distance){
       return 'This one is on me!'
       break;
     case (distance > 2000):
-      return 'I will gladly take your thirty bucks';
+      return 'I will gladly take your thirty bucks.';
+      break;
     case (distance > 2500):
       return 'No can do.'
     default:
