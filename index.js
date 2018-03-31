1012cff38388da51d3f0ccfb29b401e236e8f644
Chase <chaseit12@gmail.com>
2018-03-31 16:12:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,5 +28,16 @@ function ternaryCheckCity(city){
 }
 
 function switchOnCharmFromTip(tip){
-  
+  switch (true){
+  case (distance < 400):
+    return 'This one is on me!'
+    break;
+  case (distance > 2500):
+    return 'No can do.'
+    break;
+  case (distance > 2000):
+    return 'I will gladly take your thirty bucks.';
+    break;
+  default:
+    return ''
 }
